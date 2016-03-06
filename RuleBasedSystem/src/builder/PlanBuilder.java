aaa7e724ae6bee2d326c90d938a5b9bf344a38c4
TheMonkeyBob <themonkeybob@hotmail.com>
2016-03-06 13:59:49 -0800

    PlanBuilder now has test plan

--- RuleBasedSystem/src/builder/PlanBuilder.java
+++ RuleBasedSystem/src/builder/PlanBuilder.java
@@ -13,6 +13,7 @@ public class PlanBuilder
       buildPlan1();
       buildPlan2();
       buildPlan3();
+      buildPlan4();
    }
    
    private static void buildPlan1()
@@ -34,83 +35,103 @@ public class PlanBuilder
       //GOAL 1
       t = new ArrayList<Test>();
       
-      //Test 1
+      //Test 1-1
       nq = new ArrayList<Question>();
       bq = new ArrayList<Question>();
       na = new ArrayList<AnswerValue>();
       ba = new ArrayList<AnswerValue>();
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
+      s = "For the number 76,123,824 which digit is in the tens place?\n\n"
+            +"a) 1\tb) 2\tc) 7\td) 8\ne) 9\tf) 3\tg) 4";
+      nq.add(new Question(s, AnswerValue.B, false));
+      na.add(AnswerValue.B);
+      s = "For the number 14,723,690 which digit is in the thousands place?\n\n"
+            +"a) 2\tb) 9\tc) 6\td) 3\ne) 7\tf) 1\tg) 26";
+      nq.add(new Question(s, AnswerValue.D, false));
+      na.add(AnswerValue.D);
+      s = "For the number 69,104,784 which digit is in the hundreds place?\n\n"
+            +"a) 1\tb) 6\tc) 0\td) 8\ne) 7\tf) 9\tg) 4";
+      nq.add(new Question(s, AnswerValue.E, false));
+      na.add(AnswerValue.E);
+      s = "For the number 24,357,890 which digit is in the millions place?\n\n"
+            +"a) 1\tb) 2\tc) 7\td) 8\ne) 9\tf) 3\tg) 4";
+      nq.add(new Question(s, AnswerValue.G, false));
+      na.add(AnswerValue.G);
+      s = "For the number 12,345,567 which digit is in the ten thousands place?\n\n"
+            +"a) 3\tb) 2\tc) 0\td) 9\ne) 8\tf) 4\tg) 1";
+      nq.add(new Question(s, AnswerValue.F, false));
+      na.add(AnswerValue.F);
+      s = "For the number 7,147,896 which\nplace is the digit 4 in?\n\n"
+            +"a) Ones\t\tb) Tens\nc) Hundreds\td) Thousands\ne) Ten Thousands\nf) Hundred Thousands\ng) Millions";
+      nq.add(new Question(s, AnswerValue.E, false));
+      na.add(AnswerValue.E);
+      s = "For the number 1,928,374 which\nplace is the digit 7 in?\n\n"
+            +"a) Ones\t\tb) Tens\nc) Hundreds\td) Thousands\ne) Ten Thousands\nf) Hundred Thousands\ng) Millions";
+      nq.add(new Question(s, AnswerValue.B, false));
+      na.add(AnswerValue.B);
+      s = "For the number 9,182,736 which\nplace is the digit 9 in?\n\n"
+            +"a) Ones\t\tb) Tens\nc) Hundreds\td) Thousands\ne) Ten Thousands\nf) Hundred Thousands\ng) Millions";
+      nq.add(new Question(s, AnswerValue.G, false));
+      na.add(AnswerValue.G);
+      s = "For the number 5,412,079 which\nplace is the digit 0 in?\n\n"
+            +"a) Ones\t\tb) Tens\nc) Hundreds\td) Thousands\ne) Ten Thousands\nf) Hundred Thousands\ng) Millions";
+      nq.add(new Question(s, AnswerValue.C, false));
+      na.add(AnswerValue.C);
+      s = "For the number 3,572,108 which\nplace is the digit 8 in?\n\n"
+            +"a) Ones\t\tb) Tens\nc) Hundreds\td) Thousands\ne) Ten Thousands\nf) Hundred Thousands\ng) Millions";
       nq.add(new Question(s, AnswerValue.A, false));
       na.add(AnswerValue.A);
       a = new Attempt(na);
       t.add(new Test("Place Value", nq, a));
       
-      //Test 2
+      //Test 1-2
       nq = new ArrayList<Question>();
       bq = new ArrayList<Question>();
       na = new ArrayList<AnswerValue>();
       ba = new ArrayList<AnswerValue>();
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
-      s = "A";
-      nq.add(new Question(s, AnswerValue.A, false));
-      na.add(AnswerValue.A);
+      s = "Solve for x\n|4| = x\n\n"
+            +"a) 0\tb) 1\tc) -4\td) -1\ne) 5\tf) 3\tg) 4";
+      nq.add(new Question(s, AnswerValue.G, false));
+      na.add(AnswerValue.G);
+      s = "Solve for x\n|-5| = x\n\n"
+            +"a) 0\tb) 1\tc) -4\td) -1\ne) 5\tf) 3\tg) 4";
+      nq.add(new Question(s, AnswerValue.E, false));
+      na.add(AnswerValue.E);
+      s = "Solve for x\n-|3| = x\n\n"
+            +"a) 3\tb) 1\tc) -3\td) -1\ne) 0\tf) -4\tg) 4";
+      nq.add(new Question(s, AnswerValue.C, false));
+      na.add(AnswerValue.C);
+      s = "Solve for x\n|(-77)| = x\n\n"
+            +"a) 0\tb) 77\tc) -77\td) 7\ne) -7\tf) 1\tg) -1";
+      nq.add(new Question(s, AnswerValue.B, false));
+      na.add(AnswerValue.B);
+      s = "Solve for x\n-|-26| = x\n\n"
+            +"a) 0\tb) 2\tc) -2\td) 26\ne) -26\tf) 6\tg) -6";
+      nq.add(new Question(s, AnswerValue.E, false));
+      na.add(AnswerValue.E);
+      s = "Solve for x\n| |-69| | = x\n\n"
+            +"a) 0\tb) 69\tc) -69\td) 6\ne) -6\tf) 9\tg) -9";
+      nq.add(new Question(s, AnswerValue.B, false));
+      na.add(AnswerValue.B);
+      s = "Solve for x\n|-|(-101)| | = x\n\n"
+            +"a) 1\tb) -101\tc) -1\td) 101\ne) 11\tf) -11\tg) 10";
+      nq.add(new Question(s, AnswerValue.D, false));
+      na.add(AnswerValue.D);
+      s = "Solve for x\n-| |13| | = x\n\n"
+            +"a) 1\tb) -1\tc) 3\td) -3\ne) 13\tf) 31\tg) -13";
+      nq.add(new Question(s, AnswerValue.G, false));
+      na.add(AnswerValue.G);
+      s = "Solve for x\n-|-|77| | = x\n\n"
+            +"a) 0\tb) 77\tc) -77\td) 7\ne) -7\tf) 1\tg) -1";
+      nq.add(new Question(s, AnswerValue.C, false));
+      na.add(AnswerValue.C);
+      s = "Solve for x\n-|-|-|-271| | = x\n\n"
+            +"a) 27\tb) 271\tc) -271\td) -27\ne) 1\tf) 2\tg) 7";
+      nq.add(new Question(s, AnswerValue.C, false));
+      na.add(AnswerValue.C);
       a = new Attempt(na);
       t.add(new Test("Absolute Value", nq, a));
       
-      //test 3
+      //test 1-3
       nq = new ArrayList<Question>();
       bq = new ArrayList<Question>();
       na = new ArrayList<AnswerValue>();
@@ -1336,6 +1357,60 @@ public class PlanBuilder
       g.add(new Goal(null, t, false, "Properties of Real Numbers"));
       
       p = new LearningPlan(g, pr, "Real Numbers");
+      
+      FileIO.writeLearningPlan(p);
+   }
+   
+   private static void buildPlan4()
+   {
+      ArrayList<Question> nq;
+      ArrayList<Question> bq;
+      ArrayList<AnswerValue> na;
+      ArrayList<AnswerValue> ba;
+      Attempt a;
+      ArrayList<Test> t;
+      ArrayList<Goal> g;
+      ArrayList<PreReq> pr;
+      LearningPlan p;
+      String s;
+      
+      g = new ArrayList<Goal>();
+      pr = new ArrayList<PreReq>();
+      
+      //GOAL 1
+      t = new ArrayList<Test>();
+      
+      //Test 1-1
+      nq = new ArrayList<Question>();
+      bq = new ArrayList<Question>();
+      na = new ArrayList<AnswerValue>();
+      ba = new ArrayList<AnswerValue>();
+      s = "Test 1\nAnswer A";
+      nq.add(new Question(s, AnswerValue.A, false));
+      na.add(AnswerValue.A);
+      a = new Attempt(na);
+      t.add(new Test("Test 1", nq, a));
+      
+      g.add(new Goal(null, t, false, "Goal 1"));
+      
+      //GOAL 2
+      t = new ArrayList<Test>();
+      
+      //Test 2-1
+      nq = new ArrayList<Question>();
+      bq = new ArrayList<Question>();
+      na = new ArrayList<AnswerValue>();
+      ba = new ArrayList<AnswerValue>();
+      s = "Test 2\nAnswer B";
+      nq.add(new Question(s, AnswerValue.B, false));
+      na.add(AnswerValue.B);
+      a = new Attempt(na);
+      t.add(new Test("Test 2", nq, a));
+      
+      g.add(new Goal(null, t, false, "Goal 2"));
+      pr.add(new PreReq(g.get(0), g.get(1), "PREREQ STRING TEST"));
+      
+      p = new LearningPlan(g, pr, "TEST PLAN");
       
       FileIO.writeLearningPlan(p);
    }
