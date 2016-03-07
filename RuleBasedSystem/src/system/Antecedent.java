1663f411a0be70bb58c8e46abfcd782a5ca1fe4e
rhdabe <rhdabe@gmail.com>
2016-03-06 23:51:03 -0800

    Comments

--- RuleBasedSystem/src/system/Antecedent.java
+++ RuleBasedSystem/src/system/Antecedent.java
@@ -1,5 +1,13 @@
 package system;
 
+/**
+ * @author Rhys
+ * */
+
+/*
+ * This interface provides a single boolean method to allow testing of an antecedent condition in a rule.
+ */
+
 public interface Antecedent {
 	public boolean testAntecedent();
 
